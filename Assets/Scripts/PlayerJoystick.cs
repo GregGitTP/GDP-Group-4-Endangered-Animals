486cb546a35fc65b7c7c45c39549a58252655257
Greg <2000247C@student.tp.edu.sg>
2021-11-30 14:49:46 +0800

    Joystick working with player animations

--- Assets/Scripts/PlayerJoystick.cs
+++ Assets/Scripts/PlayerJoystick.cs
@@ -7,35 +7,40 @@ public class PlayerJoystick : MonoBehaviour
     public Joystick js;
     public float movementSpeed = 5f;
     public Animator anim;
+    public Camera mainCamera;
 
     Vector3 originalLocalScale;
     Rigidbody2D rb;
 
     private void Start(){
         rb=GetComponent<Rigidbody2D>();
+        originalLocalScale=transform.localScale;
+    }
+
+    private void Update(){
+        CameraFollow();
+        UpdateAnimation();
     }
 
     private void FixedUpdate(){
         float hori=0;
         float vert=0;
 
-        if(js.Horizontal>.5f || js.Horizontal<-.5f) hori=js.Horizontal*movementSpeed;
-        if(js.Vertical>.5f || js.Vertical<-.5f) vert=js.Vertical*movementSpeed;
+        if(js.Horizontal>.5f || js.Horizontal<-.5f){
+            hori=js.Horizontal*movementSpeed;
+        }
+        if(js.Vertical>.5f || js.Vertical<-.5f){
+            vert=js.Vertical*movementSpeed;
+        } 
 
-        rb.velocity=new Vector2(hori,vert);
-        UpdateAnimation();
+        rb.velocity=new Vector3(hori,vert,0f);
     }
 
     private void UpdateAnimation(){
-        if(js.Horizontal<.5f && js.Horizontal>-.5f){
-            ResetAnimation();
-            return;
-        }else if(js.Vertical<.5f && js.Vertical>-.5f){
-            ResetAnimation();
-            return;
-        }
+        ResetAnimation();
+        if(!(js.Horizontal>.5f || js.Horizontal<-.5f)&&!(js.Vertical>.5f || js.Vertical<-.5f)) return;
 
-        Vector2 moveVector = new Vector2(js.Horizontal, js.Vertical);
+        Vector3 moveVector = new Vector3(js.Horizontal, js.Vertical,0f);
 
         float targetAngle=0f;
 
@@ -60,31 +65,37 @@ public class PlayerJoystick : MonoBehaviour
         float offset = 45;
 
         if (targetAngle > 0f + offset && targetAngle <= 90f + offset)
-            {
-                // Character walk up
-                anim.SetBool("up", true);
-            }
-            else if (targetAngle > 90f + offset && targetAngle <= 180f + offset)
-            {
-                // Character walk left
-                anim.SetBool("side", true);
-            }
-            else if (targetAngle > 180f + offset && targetAngle <= 270f + offset)
-            {
-                // Character walk down
-                anim.SetBool("down", true);
-            }
-            else
-            {
-                // Character walk right
-                anim.SetBool("side", true);
-                transform.localScale = new Vector3(originalLocalScale.x * -1, originalLocalScale.y, 0);
-            }
+        {
+            // Character walk up
+            anim.SetBool("up", true);
+        }
+        else if (targetAngle > 90f + offset && targetAngle <= 180f + offset)
+        {
+            // Character walk left
+            anim.SetBool("side", true);
+        }
+        else if (targetAngle > 180f + offset && targetAngle <= 270f + offset)
+        {
+            // Character walk down
+            anim.SetBool("down", true);
+        }
+        else
+        {
+            // Character walk right
+            anim.SetBool("side", true);
+            transform.localScale = new Vector3(originalLocalScale.x * -1, originalLocalScale.y, 0);
+        }
     }
 
     private void ResetAnimation(){
         anim.SetBool("up",false);
         anim.SetBool("down",false);
         anim.SetBool("side",false);
+        transform.localScale=originalLocalScale;
+    }
+
+    private void CameraFollow()
+    {
+        mainCamera.transform.position = new Vector3(transform.position.x,transform.position.y,mainCamera.transform.position.z);
     }
 }
